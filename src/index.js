ce236bb86e8a7a50774208e54d02bf747c0bc32e
Rustam <ahmetov.rustam2011@gmail.com>
2020-12-08 16:26:13 +0500

    commit_with_length_control

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
+
 // Get Quote From API
 async function getQuote() {
     const proxyURL = 'http://cors-anywhere.herokuapp.com/'
@@ -5,7 +12,22 @@ async function getQuote() {
     try {
         const response = await fetch(proxyURL + apiURL)
         const data = await response.json()
-        console.log(data)
+        // If author is blank, add 'unkown'
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Неизвестный автор'
+        } else {
+            authorText.innerText = data.quoteAuthor
+        }
+
+        // Reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote')
+        } else {
+            quoteText.classList.remove('long-quote')
+        }
+        
+        quoteText.innerText = data.quoteText
+
     } catch (error) {
         getQuote()
         console.log('Ой, ничего не вышло', error)
